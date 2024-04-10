ee2ab9b9ca1f392ad8cf38b580749fdc7f92137d
lucia45370 <111748127+lucia45370@users.noreply.github.com>
2024-04-10 14:05:40 +0200

    hello

---
+++ init.js
@@ -0,0 +1,39 @@
+document.getElementsByTagName("header")[0].innerHTML = `
+<nav class="navbar navbar-light bg-light">
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <img src="love like Jesus..jpg" width="125px">
+    </button>
+    <div class="collapse  justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Appareal.html">Appareal</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="Accesories.html">Accessories</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="cart.html">
+                    <img src="shopping light brown icon app ios14.jpg">
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+`;
+
+
+document.getElementsByTagName("footer")[0].innerHTML = `
+<footer class="section-p1">
+        <div class="col">
+            <h4>Contact</h4>
+            <p><strong>Address:</strong> 500 Wellington Road, street 9, Los Angeles</p>
+            <p><strong>Phone:</strong> +01 3333 345 /(+91) 01 3542 9876</p>
+            <p><strong>Hours:</strong> 10:00 - 18:00, Mon - Sat</p>
+        </div>
+    </footer>
+`;
